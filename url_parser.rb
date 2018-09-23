579dcf5b902ed352fd6033734a641cd78bca0ef9
RumenRT <33309rt@gmail.com>
2018-09-23 13:36:30 -0400

    working url_parser

---
+++ url_parser.rb
@@ -0,0 +1,43 @@
+class UrlParser
+  def initialize(url)
+    @new_url = url
+
+  end
+
+  def scheme
+    @scheme = @new_url.split(':').first
+  end
+
+  def domain
+    @domain = @new_url.split('://').last.split(':').first
+  end
+
+  def port
+     @port = @new_url.split(":").last.split("/").first
+    if @port == "" && scheme == "http"
+      "80"
+    elsif @port == "" && scheme == "https"
+      "443"
+    else
+      @port
+    end
+  end
+
+  def path
+    @path = @new_url.split("/").last.split("?").first
+    @path == "" ? nil : @path
+  end
+
+  def query_string
+    @query_string = @new_url.split('?').last.split('#').first.split('&')
+    @query_string.map! do |item|
+      item.split("=")
+      end
+    @query_string.to_h
+  end
+
+  def fragment_id
+    @fragment_id = @new_url.split('#').last
+  end
+
+end
